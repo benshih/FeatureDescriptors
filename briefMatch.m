41b93129ceea92aa9c6aa3befa1f124165507798
Benjamin Shih <benjshih@gmail.com>
2013-10-18 15:29:39 -0400

    began part 3. still stuck on parts 1 and 2

---
+++ briefMatch.m
@@ -0,0 +1,9 @@
+% Benjamin Shih
+% 10/18/2013
+% 16720 HW3 BRIEF Feature Descriptions
+% 2.3 Assembling the BRIEF detector
+
+function [matches] = briefMatch(desc1, desc2, ratio)
+    
+
+end
---
+++ ransacH2to1.m
@@ -0,0 +1,9 @@
+% Benjamin Shih
+% 10/18/2013
+% 16720 HW3 BRIEF Feature Descriptions
+% 3.1
+
+function [bestH2to1, bestError, inliers] = ransacH2to1(matches, locs1, locs2)
+    
+
+end
